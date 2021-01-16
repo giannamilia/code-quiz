9070445df97f069f4bed92d1c70cc4eb80c6e359
Gianna <giannaa512@gmail.com>
2021-01-16 13:01:24 -0500

    final code

--- script.js
+++ script.js
@@ -1,99 +1,99 @@
 // Variables
-var startButton = document.querySelector("#start-button")
+var startButton = document.querySelector("#start-button");
 // variable for timer
+var quizSecs = 30;
 var quizTimer = setInterval(function () {
-    quizSecs--;
-    startTime.textContext = "quiz starting in" +quizSecs
+  quizSecs--;
+  // startTime.textContext = "quiz starting in" +quizSecs
 
-    if(quizSecs ---0) {
-        startTime.classList.add("hide");
-        renderQuestion();
-        quizTime();
-        stop(quizSecs);
-    }
+  if (quizSecs == 0) {
+    startTime.classList.add("hide");
+    renderQuestion();
+    quizTime();
+    stop(quizSecs);
+  }
 }, 1000);
 
 //variable for startscreen
 
 function quizTime() {
-    var counter = 10;
-    setInterval(function () {
-        counter--;
-        timer.textContext = "remaining time: " + counter;
-        if (counter === 0) {
-            clearInterval(counter);
-            quiz.classList.add("hide");
-            scorePage.classList.remove("hide");
-        }  
-    }, 1000);
+  var counter = 10;
+  setInterval(function () {
+    counter--;
+    timer.textContext = "remaining time: " + counter;
+    if (counter === 0) {
+      clearInterval(counter);
+      quiz.classList.add("hide");
+      scorePage.classList.remove("hide");
+    }
+  }, 1000);
 }
 // variable for questions
 // variable for questionIndex
-
-
-function quizTime () {
-    var counter = 10;
-    setInterval(function () {
-        counter--;
-        TimeRanges.textContext = "remaining time: " + counter;
-        if (counter === 0) {
-            clearInterval(counter);
-            quizTime.classList.add("hide");
-
-        }
-    }, 1000);
-}
 // Questions
-var questions = [
-    {
-        q: 'first question',
-        a: 'five',
-        c: ['one', 'three', 'four', 'five']
-    },
-    {
-        q: 'second question',
-        a: 'five',
-        c: ['one', 'three', 'four', 'five']
-    }, 
-    {
-        q: 'third question',
-        a: 'five',
-        c: ['one', 'three', 'four', 'five']
-    }
-    {
-        q: 'fourth question',
-        a: 'five',
-        c: ['one', 'three', 'four', 'five']
-    }, 
-    {
-        q: 'fifth question',
-        a: 'five',
-        c: ['one', 'three', 'four', 'five']
-    }
-]
+var question = [
+  {
+    q: "first question",
+    a: "five",
+    c: ["one", "three", "four", "five"],
+  },
+  {
+    q: "second question",
+    a: "five",
+    c: ["one", "three", "four", "five"],
+  },
+  {
+    q: "third question",
+    a: "five",
+    c: ["one", "three", "four", "five"],
+  },
+  {
+    q: "fourth question",
+    a: "five",
+    c: ["one", "three", "four", "five"],
+  },
+  {
+    q: "fifth question",
+    a: "five",
+    c: ["one", "three", "four", "five"],
+  },
+];
 
-questions[].c
+//question[.c]
+
+var lastQuestion = question.length - 1;
+var displayedQuestion = 0;
+function nextQuestion() {
+  displayedQuestion++;
+  renderQuestion();
+  nextQuestion;
+}
 
 // FUNCTIONS
 // Function to start timer
-function startTime() {
-
-}
 
 // Function to start the quiz
 function startQuiz() {
-    // Add class to start screen to hide it
-    // Remove class from question screen to show it
-    // Call timer function
-    startTime()
-
+  // Add class to start screen to hide it
+  // Remove class from question screen to show it
+  // Call timer function
+  
+  // quizTimer();
 }
 
-// Functoin to get question
-function getQuestion() {
+// Function to get question
+function renderQuestion() {
+  var q = question[displayedQuestion];
+  console.log(question);
+  var questionEL = document.querySelector("#question");
+  questionEL.innerHTML = "<p>" + q.question + "</p>";
+  a.innerHTML = q.a;
+  b.innerHTML = q.b;
+  c.innerHTML = q.c;
+  d.innerHTML = q.d;
 
+  console.log(question);
 }
-
 
 // Event Listeners
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startQuiz);
